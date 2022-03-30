7ef98185390ffbe4fa7ac22c68d9943989539c70
Gabriel Zayas <g-zayas@hotmail.com>
2022-03-30 10:43:34 +0900

    Cant remove or tombstone Platform Application

--- app/models/concerns/memberships/base.rb
+++ app/models/concerns/memberships/base.rb
@@ -58,7 +58,7 @@ module Memberships::Base
   end
 
   def tombstone?
-    user.nil? && invitation.nil?
+    user.nil? && invitation.nil? && !platform_agent?
   end
 
   def last_admin?
@@ -133,5 +133,9 @@ module Memberships::Base
   # members shouldn't receive notifications unless they are either an active user or an outstanding invitation.
   def should_receive_notifications?
     invitation.present? || user.present?
+  end
+
+  def platform_agent?
+    platform_agent_of_id.present?
   end
 end
