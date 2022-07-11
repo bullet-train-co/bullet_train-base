7736f0174af24d7ca5a554cd6d3c55522ae920b0
Pascal Laliberté <pascal@hey.com>
2022-07-11 17:35:00 +0000

    WIP mobile_menu: rewrite to use two reveal instances, track opens - has console.log calls

--- app/javascript/controllers/mobile_menu_controller.js
+++ app/javascript/controllers/mobile_menu_controller.js
@@ -1,24 +1,70 @@
 import { Controller } from "@hotwired/stimulus"
 
+const CLOSED = "closed"
+const OPEN = "open"
+
 export default class extends Controller {
-  static targets = [ "wrapper"]
-  static classes = [ "hidden" ] // necessary because stimulus-reveal will mess with the [hidden] attribute on the wrapper
+  static targets = [ "wrapper", "revealable"]
+  static classes = [ "hidden" ] // necessary because we're always hiding the mobile menu on larger screens and this is the class used for only mobile screen sizes
   static values = {
     showEventName: String,
     hideEventName: String,
   }
-
-  toggle() {
-    const eventName = this.isWrapperHidden? this.showEventNameValue: this.hideEventNameValue
-    if (this.isWrapperHidden) {
-      this.showWrapper()
-    }
-    
-    this.wrapperTarget.dispatchEvent(new CustomEvent(eventName))
+  
+  connect() {
+    this.addDefaultStatusPropertyToRevealableTargets()
   }
   
-  get isWrapperHidden() {
-    return this.wrapperTarget.classList.contains(this.hiddenClass)
+  dispatchEventToRevealableTargets(eventName) {
+    console.log('dispatchEventToRevealableTargets', eventName)
+    this.revealableTargets.forEach((revealableTarget) => revealableTarget.dispatchEvent(new CustomEvent(eventName)))
+  }
+
+  toggle() {
+    if (this.areAllRevealableTargetsOfStatus(CLOSED)) {
+      this.open()
+    } else if (this.areAllRevealableTargetsOfStatus(OPEN)){
+      this.close()
+    }
+    // else it's transitioning so ignore
+  }
+  
+  open() {
+    this.setStatusOfRevealableTargetsTo(CLOSED)
+    this.showWrapper()
+    this.dispatchEventToRevealableTargets(this.showEventNameValue)
+  }
+  
+  finishOpening(event) {
+    console.log('finishOpening', event.type, event.target, event.currentTarget)
+    event.currentTarget.dataset.mobileMenuRevealableTargetStatus = OPEN
+  }
+  
+  close() {
+    this.setStatusOfRevealableTargetsTo(OPEN)
+    this.dispatchEventToRevealableTargets(this.hideEventNameValue)
+  }
+  
+  finishClosing() {
+    event.currentTarget.dataset.mobileMenuRevealableTargetStatus = CLOSED
+    console.log('finishClosing', event.type, event.currentTarget.dataset.mobileMenuRevealableTargetStatus, this.areAllRevealableTargetsOfStatus(CLOSED))
+    if (this.areAllRevealableTargetsOfStatus(CLOSED)) {
+      this.hideWrapper()
+    }
+  }
+  
+  addDefaultStatusPropertyToRevealableTargets() {
+    if (!this.areAllRevealableTargetsOfStatus(CLOSED) && !this.areAllRevealableTargetsOfStatus(OPEN)) {
+      this.setStatusOfRevealableTargetsTo(CLOSED)
+    }
+  }
+  
+  setStatusOfRevealableTargetsTo(status) {
+    this.revealableTargets.forEach((revealableTarget) => revealableTarget.dataset.mobileMenuRevealableTargetStatus = status)
+  }
+  
+  areAllRevealableTargetsOfStatus(status) {
+    return this.revealableTargets.length === this.revealableTargets.filter((revealableTarget) => revealableTarget.dataset.mobileMenuRevealableTargetStatus === status).length
   }
   
   showWrapper() {
