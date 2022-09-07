dac27f84c628c7fc595af9ded0da8a2e0ac74862
Kasper Timm Hansen <kaspth@gmail.com>
2022-09-07 10:39:00 +0200

    Tune loading a bit

--- lib/bullet_train/engine.rb
+++ lib/bullet_train/engine.rb
@@ -2,10 +2,8 @@ module BulletTrain
   class Engine < ::Rails::Engine
     initializer "i18n.introspection" do
       unless Rails.env.production?
-        ActiveSupport.on_load :action_view do
-          require "bullet_train/i18n/introspection"
-          helpers.include BulletTrain::I18n::Introspection
-        end
+        require "bullet_train/i18n/introspection"
+        ActiveSupport.on_load(:action_view) { include BulletTrain::I18n::Introspection }
       end
     end
   end
