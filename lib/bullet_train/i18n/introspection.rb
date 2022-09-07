ce90af1899f067fd74146d03af3618342f3a158e
Kasper Timm Hansen <kaspth@gmail.com>
2022-09-07 10:49:16 +0200

    Short circuit our extension if we're in the mailers context; add comment for why we need the present checks.

--- lib/bullet_train/i18n/introspection.rb
+++ lib/bullet_train/i18n/introspection.rb
@@ -1,10 +1,12 @@
 module BulletTrain::I18n; end
 module BulletTrain::I18n::Introspection
   def translate(key, **options)
+    return super unless params.present? # Mailers don't have params.
+
     # When bundled Ruby gems provide a lot of translations, it can be difficult to figure out which strings in the
     # application are coming from where. To help with this, you can add `?debug=true` to any URL and we'll output
     # any rendered strings and their translation keys on the console.
-    if params.present? && (params[:log_locales] || params[:show_locales])
+    if params[:log_locales] || params[:show_locales]
       begin
         super(key + "💣", **options.except(:default))
       rescue I18n::MissingTranslationData => exception
@@ -13,17 +15,15 @@ module BulletTrain::I18n::Introspection
     end
 
     super.tap do |result|
-      if params.present?
-        if params[:log_locales]
-          if result == options[:default]
-            puts "🌐 #{full_translation_key}: Not found? Result matched default: \"#{result}\"".yellow
-          else
-            puts "🌐 #{full_translation_key}: \"#{result}\"".green
-          end
+      if params[:log_locales]
+        if result == options[:default]
+          puts "🌐 #{full_translation_key}: Not found? Result matched default: \"#{result}\"".yellow
+        else
+          puts "🌐 #{full_translation_key}: \"#{result}\"".green
         end
-
-        return full_translation_key if params[:show_locales]
       end
+
+      return full_translation_key if params[:show_locales]
     end
   end
 end
