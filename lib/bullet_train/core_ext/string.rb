59c498fd98d08c94dfa70cfa46371b61087e8c5b
Kasper Timm Hansen <kaspth@gmail.com>
2022-09-12 18:16:23 +0200

    Mark the possessive gem credit

--- lib/bullet_train/core_ext/string.rb
+++ lib/bullet_train/core_ext/string.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class String
+  # Credit goes to the "possessive" gem
+  # https://rubygems.org/gems/possessive/versions/1.0.1
   def possessive
     self + end_with?("s") ? "’" : "’s"
   end
